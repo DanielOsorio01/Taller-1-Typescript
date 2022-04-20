e5e1ee17b98f4d1c297fc513559174fe09c96311
Daniel Osorio Cardenas <d.osorioc2@uniandes.edu.co>
2022-04-20 16:45:51 -0500

    Parte 2

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { Serie } from './serie.js';
 import { dataSeries } from './data.js';
 
 let seriesTBody: HTMLElement = document.getElementById("series")!;
+let detalleSerieDiv: HTMLElement = document.getElementById("detalleSerie")!;
 
 const totalSeasonAvgElm: HTMLElement = document.getElementById("seasonsAvg")!;
 
@@ -10,20 +11,59 @@ renderSeriesInTable(dataSeries);
 
 totalSeasonAvgElm.innerHTML = `Season Average: ${getSeasonsAverage(dataSeries)}`;
 
-function renderSeriesInTable(series: Serie[]) {
+document.addEventListener("DOMContentLoaded", () => {
+    const rows: NodeListOf<HTMLElement> = document.querySelectorAll("tr");
+    console.log(rows);
+});
+
+function renderSeriesInTable(series: Serie[]): void {
     console.log('Desplegando series');
     series.forEach((serie) =>{
         let trElement = document.createElement("tr");
-        trElement.innerHTML =  `<td>${serie.id}</td>
-                                <td class="name">${serie.name}</td>
-                                <td>${serie.channel}</td>
-                                <td>${serie.seasons}</td>`;
+        trElement.onclick = () => onSelected(serie);
+        trElement.innerHTML =  `<td id="id">${serie.id}</td>
+                                <td id="name">${serie.name}</td>
+                                <td id="channel">${serie.channel}</td>
+                                <td id="seasons">${serie.seasons}</td>`;
         seriesTBody.appendChild(trElement);
     });
 }
-function getSeasonsAverage(series: Serie[]) {
+
+function onSelected(serie: Serie): void{
+    if (detalleSerieDiv.firstChild != null){
+        detalleSerieDiv.removeChild(detalleSerieDiv.firstChild);
+    }
+    console.log(serie);
+    let cardElement = document.createElement("div");
+    cardElement.setAttribute("class","card");
+    cardElement.setAttribute("style","width: 18rem;");
+    cardElement.innerHTML = `   <img src="${serie.img}" class="card-img-top" alt="imagen">
+                                <div class="card-body">
+                                <h5>${serie.name}</h5>
+                                <p class="card-text">${serie.description}</p>
+                                <a href="url">${serie.link}</a>
+                                </div>`;
+    detalleSerieDiv.appendChild(cardElement);
+}
+
+function getSeasonsAverage(series: Serie[]): number {
     let sum: number = 0;
     series.forEach((serie) => sum += serie.seasons);
     return series.length === 0 ? 0 : sum/series.length;
 }
+
+function getSerieById(series: Serie[], id: number): Serie {
+    let serieEncontrada: Serie;
+    let terminado: boolean = false;
+    let i: number = 0;
+    while (!terminado){
+        if (series[i].id === id){
+            serieEncontrada = series[i];
+            terminado = true;
+        }
+        i += 1;
+    }
+    return serieEncontrada!;
+}
+
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,48 @@
 import { dataSeries } from './data.js';
 var seriesTBody = document.getElementById("series");
+var detalleSerieDiv = document.getElementById("detalleSerie");
 var totalSeasonAvgElm = document.getElementById("seasonsAvg");
 renderSeriesInTable(dataSeries);
 totalSeasonAvgElm.innerHTML = "Season Average: ".concat(getSeasonsAverage(dataSeries));
+document.addEventListener("DOMContentLoaded", function () {
+    var rows = document.querySelectorAll("tr");
+    console.log(rows);
+});
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                                <td class=\"name\">").concat(serie.name, "</td>\n                                <td>").concat(serie.channel, "</td>\n                                <td>").concat(serie.seasons, "</td>");
+        trElement.onclick = function () { return onSelected(serie); };
+        trElement.innerHTML = "<td id=\"id\">".concat(serie.id, "</td>\n                                <td id=\"name\">").concat(serie.name, "</td>\n                                <td id=\"channel\">").concat(serie.channel, "</td>\n                                <td id=\"seasons\">").concat(serie.seasons, "</td>");
         seriesTBody.appendChild(trElement);
     });
+}
+function onSelected(serie) {
+    if (detalleSerieDiv.firstChild != null) {
+        detalleSerieDiv.removeChild(detalleSerieDiv.firstChild);
+    }
+    console.log(serie);
+    var cardElement = document.createElement("div");
+    cardElement.setAttribute("class", "card");
+    cardElement.setAttribute("style", "width: 18rem;");
+    cardElement.innerHTML = "   <img src=\"".concat(serie.img, "\" class=\"card-img-top\" alt=\"imagen\">\n                                <div class=\"card-body\">\n                                <h5>").concat(serie.name, "</h5>\n                                <p class=\"card-text\">").concat(serie.description, "</p>\n                                <a href=\"url\">").concat(serie.link, "</a>\n                                </div>");
+    detalleSerieDiv.appendChild(cardElement);
 }
 function getSeasonsAverage(series) {
     var sum = 0;
     series.forEach(function (serie) { return sum += serie.seasons; });
     return series.length === 0 ? 0 : sum / series.length;
+}
+function getSerieById(series, id) {
+    var serieEncontrada;
+    var terminado = false;
+    var i = 0;
+    while (!terminado) {
+        if (series[i].id === id) {
+            serieEncontrada = series[i];
+            terminado = true;
+        }
+        i += 1;
+    }
+    return serieEncontrada;
 }
